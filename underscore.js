bcaa6499e8cbeec697ddba88a40d0380a4d35830
ahonn <ahonn95@outlook.com>
2016-08-05 22:39:15 +0800

    add some array function comments

--- underscore.js
+++ underscore.js
@@ -585,11 +585,14 @@
   };
 
   // Array Functions
+  // 数组函数
   // ---------------
 
   // Get the first element of an array. Passing **n** will return the first N
   // values in the array. Aliased as `head` and `take`. The **guard** check
   // allows it to work with `_.map`.
+  // `first` 函数，亦称为 `head`, `take`。
+  // 返回数组的第 n 个元素，当无 n 参数时，默认返回第一个元素。
   _.first = _.head = _.take = function(array, n, guard) {
     if (array == null) return void 0;
     if (n == null || guard) return array[0];
@@ -599,12 +602,16 @@
   // Returns everything but the last entry of the array. Especially useful on
   // the arguments object. Passing **n** will return all the values in
   // the array, excluding the last N.
+  // `initial` 函数。
+  // 删除数组的后 n 个元素，当无 n 参数时，默认删除最后一个元素。
   _.initial = function(array, n, guard) {
     return slice.call(array, 0, Math.max(0, array.length - (n == null || guard ? 1 : n)));
   };
 
   // Get the last element of an array. Passing **n** will return the last N
   // values in the array.
+  // `last` 函数。
+  // 返回数组的后 n 个元素，当无 n 参数时，默认返回最后一个元素。
   _.last = function(array, n, guard) {
     if (array == null) return void 0;
     if (n == null || guard) return array[array.length - 1];
@@ -614,22 +621,32 @@
   // Returns everything but the first entry of the array. Aliased as `tail` and `drop`.
   // Especially useful on the arguments object. Passing an **n** will return
   // the rest N values in the array.
+  // `rest` 函数，，亦称为 `tail`, `drop`。
+  // 删除数组中的前 n 个元素，当无 n 参数是，默认删除数组的第一个元素。
   _.rest = _.tail = _.drop = function(array, n, guard) {
     return slice.call(array, n == null || guard ? 1 : n);
   };
 
   // Trim out all falsy values from an array.
+  // `compact` 函数。
+  // 返回一个新的数组，包含 array 中的所有非 false 项。
   _.compact = function(array) {
     return _.filter(array, _.identity);
   };
 
   // Internal implementation of a recursive `flatten` function.
+  // `flatten` 函数的内部实现。
+  // input 为需要展开的数组，shallow 为是否需要浅展开。
+  // strict 为当 shallow 为 true 时，严格过滤数组中的非数组项。
+  // output 为输出的数组。
   var flatten = function(input, shallow, strict, startIndex) {
     var output = [], idx = 0;
     for (var i = startIndex || 0, length = getLength(input); i < length; i++) {
       var value = input[i];
+      // 检查 input 中的项是否为 数组 或者 arguments
       if (isArrayLike(value) && (_.isArray(value) || _.isArguments(value))) {
         //flatten current level of array or arguments object
+        // 如果为深展开，递归调用 flatten 函数
         if (!shallow) value = flatten(value, shallow, strict);
         var j = 0, len = value.length;
         output.length += len;
@@ -637,6 +654,8 @@
           output[idx++] = value[j++];
         }
       } else if (!strict) {
+        // 若该项非数组或 arguments, 且为不严格过滤展开，不做任何处理
+        // 即严格过滤非数组项并且是深展开时，最后得到的 output 为空数组
         output[idx++] = value;
       }
     }
@@ -644,11 +663,15 @@
   };
 
   // Flatten out an array, either recursively (by default), or just one level.
+  // `flatten` 函数。
+  // 展开数组，默认不过滤非数组项
   _.flatten = function(array, shallow) {
     return flatten(array, shallow, false);
   };
 
   // Return a version of the array that does not contain the specified value(s).
+  // `without` 函数。
+  // 返回一个删除某些特定项后的新数组。
   _.without = function(array) {
     return _.difference(array, slice.call(arguments, 1));
   };
